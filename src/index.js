c433624c2e78ec8d4800c98d2e107e8885fdba10
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-08-20 16:21:15 -0400

    import semantic.min.css rather than injecting the cdn link

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-// import App from './components/App';
-import TopStories from './components/TopStories';
+import App from './components/App';
+import 'semantic-ui-css/semantic.min.css';
 
-const styleLink = document.createElement('link');
-styleLink.rel = 'stylesheet';
-styleLink.href =
-  'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
-document.head.appendChild(styleLink);
-
-ReactDOM.render(<TopStories />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
