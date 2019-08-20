6af77535a1b724ce610755e6dc720dc421c7e35f
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-08-20 15:20:18 -0400

    Create and inject semantic ui css in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import App from './components/App';
+// import App from './components/App';
+import TopStories from './components/TopStories';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const styleLink = document.createElement('link');
+styleLink.rel = 'stylesheet';
+styleLink.href =
+  'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
+document.head.appendChild(styleLink);
+
+ReactDOM.render(<TopStories />, document.getElementById('root'));
 serviceWorker.unregister();
