cd0828a5a978ede8f5d94ad396e55a64af01de6c
Manikandan Sundararajan <MSundararajan@Derivetech.com>
2019-08-20 10:17:09 -0400

    Refactor to use commonjs require syntax

--- server/index.js
+++ server/index.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import { json } from 'body-parser';
-import cors from 'cors';
-import { join } from 'path';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const topstories = require('./routes/topstories');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(json());
-app.use(cors());
-app.use(express.static('build'));
+app.use(bodyParser.json(), cors(), express.static('build'));
+app.use('/topstories', topstories);
 
-app.get('/ping', (req, res) => res.send('pong'));
-app.get('/', (req, res) => {
-  res.sendFile(join('build', 'index.html'));
+app.get('/ping', (_req, res) => res.send('pong'));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join('build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port - ${port}`);
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
 });
