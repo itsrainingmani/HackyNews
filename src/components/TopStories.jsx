6ce3c337a196cfdec32e2857a09ecaff52a1477a
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-08-20 15:20:33 -0400

    Component that returns List of Top Stories

---
+++ src/components/TopStories.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Container, List, Button } from 'semantic-ui-react';
+
+function StoryList(props) {
+  return (
+    <List relaxed verticalAlign="middle">
+      {props.storyList.map(story => (
+        <List.Item key={story.id}>
+          <List.Content>
+            <List.Header as="a">
+              <a href={story.url}>{story.title}</a>
+            </List.Header>
+            by {story.by}
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  );
+}
+
+export default function TopStories() {
+  const [topStories, setTopStories] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function getTopStories() {
+      let response = await fetch(`/stories/top/${page}`);
+      let storylist = await response.json();
+
+      console.log(storylist);
+      if (!response.ok) throw Error('Something went wrong');
+      else setTopStories(storylist);
+    }
+    getTopStories();
+  }, [page]);
+
+  return (
+    <Container>
+      <StoryList storyList={topStories} />
+      <Button onClick={() => setPage(page + 1)}>More</Button>
+    </Container>
+  );
+}
