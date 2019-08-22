9ca650fecf2fd0fdc80f13b87e799cd6db2104d3
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-08-22 15:52:14 -0400

    Moved reusable components to StoryUtils and simplify design of Stories

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,26 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
-import StoryList from './StoryDisplay';
-import { getSelectStories } from '../hackerapi';
+import StoryList, { StoryButton, GetTopStories } from './StoryUtils';
 
 export default function Stories(props) {
   const [topStories, setTopStories] = useState([]);
 
   useEffect(() => {
-    const getTopStories = async () => {
-      try {
-        let storylist = await getSelectStories(props.type, props.page);
-        storylist = storylist.filter(story => story !== null);
-        console.log(storylist);
-        return storylist;
-      } catch (err) {
-        return [];
-      }
-    };
-
-    getTopStories()
+    GetTopStories(props.type, props.page)
       .then(sl => setTopStories(sl))
       .catch(sl => setTopStories(sl));
   }, [props.type, props.page]);
@@ -28,15 +14,7 @@ export default function Stories(props) {
   return (
     <ErrorBoundary>
       <StoryList storyList={topStories} />
-      <Button>
-        {props.type !== 'top' ? (
-          <Link to={`/${props.type}/${(parseInt(props.page) || 1) + 1}`}>
-            More
-          </Link>
-        ) : (
-          <Link to={`/${(parseInt(props.page) || 1) + 1}`}>More</Link>
-        )}
-      </Button>
+      <StoryButton type={props.type} page={props.page} />
     </ErrorBoundary>
   );
 }
